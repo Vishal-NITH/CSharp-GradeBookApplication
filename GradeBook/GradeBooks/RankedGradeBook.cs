32094bab30aa8e6a10452e2ee20f2a4c1b40f285
Vishal Agarawal <viagaraw@microsoft.com>
2020-08-30 18:44:34 +0530

    Adding null checks to Students list object

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -47,7 +47,7 @@ namespace GradeBook.GradeBooks
 
         public override void CalculateStatistics()
         {
-            if (Students.Count < 5)
+            if (Students != null && Students.Count < 5)
             {
                 Console.WriteLine("Ranked grading requires at least 5 students with grades in" +
                     " order to properly calculate a student's overall grade.");
@@ -57,7 +57,7 @@ namespace GradeBook.GradeBooks
 
         public override void CalculateStudentStatistics(string name)
         {
-            if (Students.Count < 5)
+            if (Students != null && Students.Count < 5)
             {
                 Console.WriteLine("Ranked grading requires at least 5 students with grades in " +
                     "order to properly calculate a student's overall grade.");
