a4406ad7e5bd966173f03e6efd5e0e4c413b9473
Vishal Agarawal <viagaraw@microsoft.com>
2020-08-30 19:37:53 +0530

    Returning in case of unknown Grade book type

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -55,6 +55,7 @@ namespace GradeBook.UserInterfaces
             else
             {
                 Console.WriteLine($"{type} is not a supported type of gradebook, please try again");
+                return;
             }
 
             Console.WriteLine("Created gradebook {0}.", name);
