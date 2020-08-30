f8eba0f16af1dceb61769fa778cc05eeb86d05a7
Vishal Agarawal <viagaraw@microsoft.com>
2020-08-30 18:28:44 +0530

    Added functionality to method GetLetterGrade to return correct grade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 
 namespace GradeBook.GradeBooks
 {
@@ -10,6 +11,40 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        private static int CompareStudentByGrade(Student s1, Student s2)
+        {
+            return s1.AverageGrade.CompareTo(s2.AverageGrade);
+        }
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+            Students.Sort(CompareStudentByGrade);
+
+            if (averageGrade > Students[Students.Count / 5].AverageGrade)
+            {
+                return 'A';
+            }
+            else if (averageGrade > Students[2 * Students.Count / 5].AverageGrade)
+            {
+                return 'B';
+            }
+            else if (averageGrade > Students[3 * Students.Count / 5].AverageGrade)
+            {
+                return 'C';
+            }
+            else if (averageGrade > Students[4 * Students.Count / 5].AverageGrade)
+            {
+                return 'D';
+            }
+                   
+            
+
+            return 'F';
         }
     }
 }
