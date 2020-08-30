51f9835ae449acb5b147a34a8db4f0f963efdf6f
Vishal Agarawal <viagaraw@microsoft.com>
2020-08-30 18:42:18 +0530

    Adding check of atleast 5 students in overidden calculate statistics methods

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -10,6 +10,7 @@ namespace GradeBook.GradeBooks
     {
         public RankedGradeBook(string name) : base(name)
         {
+            Students.Sort(CompareStudentByGrade);
             Type = GradeBookType.Ranked;
         }
 
@@ -23,7 +24,6 @@ namespace GradeBook.GradeBooks
             {
                 throw new InvalidOperationException();
             }
-            Students.Sort(CompareStudentByGrade);
 
             if (averageGrade > Students[Students.Count / 5].AverageGrade)
             {
@@ -43,6 +43,26 @@ namespace GradeBook.GradeBooks
             }
 
             return 'F';
+        }
+
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in" +
+                    " order to properly calculate a student's overall grade.");
+            }
+            base.CalculateStatistics();
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in " +
+                    "order to properly calculate a student's overall grade.");
+            }
+            base.CalculateStudentStatistics(name);
         }
     }
 }
