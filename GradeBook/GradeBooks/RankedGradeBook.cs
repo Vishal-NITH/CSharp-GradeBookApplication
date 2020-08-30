964c84280011a43588d0a3653a51298d9ee6dea9
Vishal Agarawal <viagaraw@microsoft.com>
2020-08-30 18:46:32 +0530

    Forgot to return if students count is less then 5

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -47,20 +47,22 @@ namespace GradeBook.GradeBooks
 
         public override void CalculateStatistics()
         {
-            if (Students != null && Students.Count < 5)
+            if (Students.Count < 5)
             {
                 Console.WriteLine("Ranked grading requires at least 5 students with grades in" +
                     " order to properly calculate a student's overall grade.");
+                return;
             }
             base.CalculateStatistics();
         }
 
         public override void CalculateStudentStatistics(string name)
         {
-            if (Students != null && Students.Count < 5)
+            if (Students.Count < 5)
             {
                 Console.WriteLine("Ranked grading requires at least 5 students with grades in " +
                     "order to properly calculate a student's overall grade.");
+                return;
             }
             base.CalculateStudentStatistics(name);
         }
